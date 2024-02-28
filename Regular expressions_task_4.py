86677cb59f6076f40f7cac02e83fbef14d5dd747
Pazukhina Marina <mgorchakova14@gmail.com>
2024-02-28 16:34:10 +0300

    Regular expressions_task_4

--- Regular expressions_task_4.py
+++ Regular expressions_task_4.py
@@ -5,10 +5,10 @@ import re
 tweet = '''Good advice! RT @TheNextWeb: What I would do differently if I was learning to
 code today https://t.co/lbwej0pxOd cc: @garybernhardt #rstats'''
 
-tweet = re.sub('http\S+\s*', '', tweet) # удалит URL
-tweet = re.sub('RT|cc', '', tweet) # удалит RT и cc
-tweet = re.sub('#\S+', '', tweet) # удалит хештеги
-tweet = re.sub('@\S+', '', tweet) # удалит упоминани
-tweet = re.sub('[%s]' % re.escape("""!"#$%&'()*+,-./:;<=>?@[\]^_`{|}~"""), '', tweet) # удалит символы пунктуации
-tweet = re.sub('\s+', ' ', tweet) # заменит пробельные символы на 1 пробел
+tweet = re.sub('http\S+\s*', '', tweet)
+tweet = re.sub('RT|cc', '', tweet)
+tweet = re.sub('#\S+', '', tweet)
+tweet = re.sub('@\S+', '', tweet)
+tweet = re.sub('[%s]' % re.escape("""!"#$%&'()*+,-./:;<=>?@[\]^_`{|}~"""), '', tweet)
+tweet = re.sub('\s+', ' ', tweet)
 print(tweet)
